dbf541de4d4268a9a27f4794196ca96354ce499c
Charles Opute Odili <chaluwa@gmail.com>
2023-07-16 07:04:00 +0000

    invoke scryfall API

--- server/searchservice/src/search.route.ts
+++ server/searchservice/src/search.route.ts
@@ -1,16 +1,26 @@
 import axios from 'axios';
 
 import type { Request, Response } from 'express';
-import type { APIResponse } from '../../../api/sdk/model/aPIResponse';
+import type { QueryResult } from '../../../api/sdk/model/queryResult';
+import type { APIResponseError } from '../../../api/sdk/model/aPIResponseError';
+
+import { SortByParam } from '../../../api/sdk/model/sortByParam';
+import { OrderByParam } from '../../../api/sdk/model/orderByParam';
+
+type APIResponse = APIResponseError | QueryResult
 
 const searchEndpoint = async (req: Request, res: Response) => {
-    const query = req.query['q'];
     let output: APIResponse;
+    const ScryfallAPI = process.env['ScryfallBase'];
+
+    const term = req.query['term'];
+    const sortBy = `${req.query['sortby'] || SortByParam.Auto}`.toLowerCase();
+    const orderBy = `${req.query['orderBy'] || OrderByParam.Name}`.toLowerCase();
+    
     try {
-      const url = `https://api.scryfall.com/cards/search?q=${query}`;
+      const url = `${ScryfallAPI}/cards/search?q=${term}&order=${orderBy}&dir=${sortBy}`;
       const { status, data} = await axios.get(url);
-  
-      console.log(status);
+      console.warn(status);
       output = data;
     } catch (error) {
       console.warn(error);
