230058bf36ca10aa4596a0aa6d4c0bcd2162ce09
Charles Opute Odili <chaluwa@gmail.com>
2023-07-16 07:03:26 +0000

    use automatice request validation

--- server/searchservice/src/index.ts
+++ server/searchservice/src/index.ts
@@ -1,4 +1,23 @@
-import expressapp from 'expressapp';
-import router from './routes';
+import fs from 'fs';
+import path from 'path';
+import dotenv  from "dotenv"
 
-expressapp(router);
+import * as jsyaml from 'js-yaml';
+import * as OpenApiValidator from 'express-openapi-validator';
+
+import router from './routes';
+import expressapp from 'expressapp';
+
+dotenv.config();
+
+const ymlFilePath = path.join('src', 'api.yaml');
+const ymlAsString = fs.readFileSync(ymlFilePath, 'utf8');
+const apiSpecYaml = jsyaml.load(ymlAsString) as string;
+
+const autoValidateRequestsMW = OpenApiValidator.middleware({
+    apiSpec: apiSpecYaml,
+    validateRequests: true, // (default)
+    validateResponses: false // false by default
+});
+
+expressapp(router, autoValidateRequestsMW);
