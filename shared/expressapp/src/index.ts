0934dba2e072c8ce8f7128a07efdacdbba593e30
Charles Opute Odili <chaluwa@gmail.com>
2023-07-16 07:02:04 +0000

    use middlewares and global error handler

--- shared/expressapp/src/index.ts
+++ shared/expressapp/src/index.ts
@@ -1,6 +1,9 @@
-import express, { Router }  from "express";
+import type { Request, Response, NextFunction } from 'express';
+type MWFunction = (req: Request, res: Response, next: NextFunction) => void;
+
+import express, { Router }  from 'express';
 import cors from 'cors';
-import log from "logger";
+import log from 'logger';
 
 import morgan from './config/morgan';
 
@@ -11,15 +14,30 @@ const randomPort = (min = 3000, max = 6000) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-const expressApp = (router: Router, _mw?: string[]) => {
+const expressApp = (router: Router, preRouteMWs: MWFunction[]) => {
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use(morgan);
 
-  // TODO add express-openapi-validator
-
+  for (const mw of preRouteMWs) {
+    app.use('/', mw);
+  }
   app.use('/', router);
+
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    // format error
+    const error = err || {};
+    err.status = err.status || 500;
+    if (err.errors) {
+      
+    }
+
+    res.status(error.status).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  });
 
   const port = process.env['PORT'] || randomPort();
   app.listen(port, () => {
