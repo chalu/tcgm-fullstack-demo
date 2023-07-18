9e9dbe53f2a47e2502371fb5bc2228af2e6f002d
Charles Opute Odili <chaluwa@gmail.com>
2023-07-18 18:03:28 +0000

    make search field more colorful

--- client/r-seeker/src/components/SearchBar.tsx
+++ client/r-seeker/src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar = ({submitHandler, inputHandler}) => {
         <form className="flex items-center ml-5 mt-5 mb-5" onSubmit={submitHandler}>
             <label htmlFor="search" className="sr-only">Search</label>
             <div className="relative">
-                <input className="max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                <input className="max-w-sm bg-gray-50 border text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                     type="text"
                     id="search" 
                     minLength={3}
