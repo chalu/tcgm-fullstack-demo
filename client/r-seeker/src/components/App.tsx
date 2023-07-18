f8466120a109af97f4b3bb171dd3c1c0f7cfd087
Charles Odili <chaluwa@gmail.com>
2023-07-18 09:03:00 -0600

    make search component work

--- client/r-seeker/src/components/App.tsx
+++ client/r-seeker/src/components/App.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 
-// import Card from './Card';
-import Footer from './Footer';
+// import Footer from './Footer';
 import SearchBar from './SearchBar';
-import ResultsView from './ResultsView';
+// import ResultsView from './ResultsView';
+
+import performSearch from '../core/engine';
 
 const App = () => {
+    const [term, setTerm] = React.useState(null);
+    const [query, setSearchQuery] = React.useState(null);
+    const [results, setResults] = React.useState([]);
+
+    const validationPtrn = /^\w+$/;
+    const handleSubmission = (event) => {
+        event.preventDefault();
+
+        if (validationPtrn.test(term || '') !== true) return;
+        setSearchQuery(term);
+        console.log(term);
+    };
+
+    const handleNav = (navTo) => {
+        return (event) => {
+            console.log(navTo);
+            performSearch(query, setResults, navTo)
+            .catch((e) => { console.warn(e) });
+        }
+    };
+
+    React.useEffect(() => {
+        if (!query || query === '') return;
+        performSearch(query, setResults)
+        .catch((e) => { console.warn(e) });
+    }, [query]);
+
     return (
         <>
-            <SearchBar />
-            <ResultsView />
-            <Footer />
+            <SearchBar submitHandler={handleSubmission} inputHandler={setTerm} />
+            {/* <ResultsView searchResults={results} /> */}
+            {/* <Footer searchResults={results} navHandler={handleNav} /> */}
         </>
     );
 }
--- client/r-seeker/src/components/SearchBar.tsx
+++ client/r-seeker/src/components/SearchBar.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 
-const SearchBar = () => {
-    const [term, setTerm] = React.useState('');
-    const validationPtrn = /^\w+$/;
-    const handleChange = ({target}) => {
-        setTerm(target.value);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        if (validationPtrn.test(term) !== true) return;
-        console.log(term);
-    };
-
+const SearchBar = ({submitHandler, inputHandler}) => {
     return (
-        <form className="flex items-center ml-5 mt-5 mb-5" onSubmit={handleSubmit}>
+        <form className="flex items-center ml-5 mt-5 mb-5" onSubmit={submitHandler}>
             <label htmlFor="search" className="sr-only">Search</label>
             <div className="relative">
                 <input className="max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                     type="text"
                     id="search" 
                     minLength={3}
-                    placeholder="try pokey or red" 
-                    value={term}
-                    onChange={handleChange}
+                    placeholder="try pokey or red"
+                    onChange={evt => inputHandler(evt.target.value)}
                     required />
             </div>
             <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
