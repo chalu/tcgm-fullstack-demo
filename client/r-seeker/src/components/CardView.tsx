247760ec3080212deaa40b2f8fca176b47443b50
Charles Opute Odili <chaluwa@gmail.com>
2023-07-18 17:27:46 +0000

    fix styles initially only targeting dark mode

--- client/r-seeker/src/components/CardView.tsx
+++ client/r-seeker/src/components/CardView.tsx
@@ -16,19 +16,19 @@ const CardView = ({ id, name, games, prices, img = 'https://placehold.co/146x204
         }, []);
     return (
         <div id={id}
-            className="ml-5 max-w-xs bg-white pt-0 border border-gray-200 rounded-lg shadow dark:border-gray-700 dark:bg-gray-800">
+            className="ml-5 max-w-xs max-h-fit bg-gray-800 pt-0 border rounded-lg shadow border-gray-700">
                 <div className='bg-white pb-1 pt-1'>
                     <img className="rounded-t-lg m-2 bg-white m-auto mt-2.5 mb-2.5 block" src={img} alt={name} />
                 </div>
     
-            <div className="max-w-xs p-5 dark:bg-gray-800">
-                <h5 className="mb-2 text-1xl break-words font-bold tracking-tight text-gray-900 dark:text-white">
+            <div className="max-w-xs p-5 bg-gray-800">
+                <h5 className="mb-2 text-1xl break-words font-bold tracking-tight text-gray-900 text-white">
                     {name}
                 </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-300">
                     Games: {(games && games.join(' | ')) || 'N/A'}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-300">
                     Prices: {(priceFigures && priceFigures.join(' | ')) || 'N/A'}
                 </p>
             </div>
