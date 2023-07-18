6e2d5a6d191d2556a4922e1533ac57fc4264c9fd
Charles Odili <chaluwa@gmail.com>
2023-07-18 03:34:54 -0600

    rename vanilla JS seeker client to v-seeker

---
+++ client/v-seeker/src/utils.js
@@ -0,0 +1,14 @@
+export const wait = ({until} = { until: 1000 }) => new Promise((resolve) => {
+    setTimeout(() => {
+        return resolve();
+    }, until);
+});
+
+export const settle = (tailPromise) => {
+    return {
+        async after(headPromise) {
+            await headPromise;
+            await tailPromise;
+        }
+    }
+};
